e22a04aced3f8ceee2370a507fbedb3fa09e9b53
albonire <andersonfabygonzalez.21@gmail.com>
2025-06-30 22:44:50 -0500

    Refactor level advancement logic in WordGenerator to improve clarity

--- Assets/WordGenerator.cs
+++ Assets/WordGenerator.cs
@@ -43,7 +43,7 @@ public class WordGenerator : MonoBehaviour {
     public static event Action<int, string[]> OnLevelAdvanced; // Evento que se dispara al avanzar de nivel
 
     private static void AdvanceLevel() {
-        currentLevel++; //para el nivel
+        currentLevel++; 
         wordsCompletedInLevel = 0;
         // En lugar de Debug.Log, disparamos el evento
         OnLevelAdvanced?.Invoke(currentLevel, learningLevels[currentLevel]);
