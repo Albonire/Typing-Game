0c220e4aff70d001668c877915aeea748555bb47
albonire <andersonfabygonzalez.21@gmail.com>
2025-07-01 22:29:39 -0500

    Refactor learning levels and level words in WordGenerator for improved structure and clarity

--- Assets/WordGenerator.cs
+++ Assets/WordGenerator.cs
@@ -6,20 +6,30 @@ using System; // Necesario para Action
 public class WordGenerator : MonoBehaviour {
 
     private static Dictionary<int, string[]> learningLevels = new Dictionary<int, string[]>() {
-        {1, new string[] {"j", "f"}}, // Nivel 1: j y f
-        {2, new string[] {"j", "f", "k", "d"}}, // Nivel 2: añade k y d
-        {3, new string[] {"j", "f", "k", "d", "g", "h"}}, // Nivel 3: añade g y h
-        {4, new string[] {"j", "f", "k", "d", "g", "h", "s", "l"}}, // Nivel 4: añade s y l
-        {5, new string[] {"j", "f", "k", "d", "g", "h", "s", "l", "a", ";"}}, // Nivel 5: añade a y ;
+        {1, new string[] {"f", "j"}},
+        {2, new string[] {"f", "j", "d", "k"}},
+        {3, new string[] {"f", "j", "d", "k", "s", "l"}},
+        {4, new string[] {"f", "j", "d", "k", "s", "l", "a", ";"}},
+        {5, new string[] {"f", "j", "d", "k", "s", "l", "a", ";", "g", "h"}},
+        {6, new string[] {"f", "j", "d", "k", "s", "l", "a", ";", "g", "h", "q", "p"}},
+        {7, new string[] {"f", "j", "d", "k", "s", "l", "a", ";", "g", "h", "q", "p", "r", "u"}},
+        {8, new string[] {"f", "j", "d", "k", "s", "l", "a", ";", "g", "h", "q", "p", "r", "u", "e", "i"}},
+        {9, new string[] {"f", "j", "d", "k", "s", "l", "a", ";", "g", "h", "q", "p", "r", "u", "e", "i", "t", "y"}},
+        {10, new string[] {"f", "j", "d", "k", "s", "l", "a", ";", "g", "h", "q", "p", "r", "u", "e", "i", "t", "y", "w", "o"}}
     };
 
 
     private static Dictionary<int, string[]> levelWords = new Dictionary<int, string[]>() {
         {1, new string[] {"jf", "fj", "jj", "ff"}},
         {2, new string[] {"jk", "fd", "kf", "dj", "jkfd", "fdjk"}},
-        {3, new string[] {"jkg", "fdh", "ghj", "hfk", "jkfdgh", "ghfdjk"}},
-        {4, new string[] {"jksl", "fdsl", "sljk", "slfd", "jkfdsl", "slfdjk"}},
-        {5, new string[] {"jksla", "fdsl;", "asljk", ";slfd", "jkfdsla", "slfdjk;"}}
+        {3, new string[] {"jks", "fdl", "sljk", "slfd", "jkfdsl", "slfdjk"}},
+        {4, new string[] {"jksa", "fdl;", "asljk", ";slfd", "jkfdsla", "slfdjk;"}},
+        {5, new string[] {"jksagh", "fdlgh;", "ghasljk", ";slfdgh", "jkfdslag", "slfdjk;gh"}},
+        {6, new string[] {"jksaghq", "fdlgh;p", "qpasljk", ";slfdghq", "jkfdslagq", "slfdjk;ghp"}},
+        {7, new string[] {"jksaghqr", "fdlgh;pu", "ruqpasljk", ";slfdghqru", "jkfdslagqu", "slfdjk;ghpr"}},
+        {8, new string[] {"jksaghqre", "fdlgh;pui", "eiqpasljk", ";slfdghqrei", "jkfdslagqei", "slfdjk;ghpre"}},
+        {9, new string[] {"jksaghqret", "fdlgh;puiy", "tyiqpasljk", ";slfdghqrety", "jkfdslagqety", "slfdjk;ghprty"}},
+        {10, new string[] {"jksaghqretw", "fdlgh;puiyo", "woyiqpasljk", ";slfdghqretwo", "jkfdslagqetwo", "slfdjk;ghprtwo"}}
     };
 
     private static int currentLevel = 1;
